2314e3519adafc9882d2d8d4d678b95451d5cf08
vauzan <vauzanmagician@gmail.com>
2019-12-07 22:17:05 +0700

    first commit

---
+++ app/src/main/java/com/vauzan/pertemuan_11/NoteViewMode.kt
@@ -0,0 +1,28 @@
+package com.vauzan.pertemuan_11
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.vauzan.pertemuan_11.catatan.Note
+import com.vauzan.pertemuan_11.catatan.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+    private var repository: NoteRepository =
+        NoteRepository(application)
+    private var allNotes: LiveData<List<Note>> = repository.getAllNotes()
+    fun insert(note: Note) {
+        repository.insert(note)
+    }
+    fun update(note: Note) {
+        repository.update(note)
+    }
+    fun delete(note: Note) {
+        repository.delete(note)
+    }
+    fun deleteAllNotes() {
+        repository.deleteAllNotes()
+    }
+    fun getAllNotes(): LiveData<List<Note>> {
+        return allNotes
+    }
+}
---
+++ app/src/main/java/com/vauzan/pertemuan_11/activity_add_note.kt
@@ -0,0 +1,12 @@
+package com.vauzan.pertemuan_11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_add_note : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
